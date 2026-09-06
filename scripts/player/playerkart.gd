3817911ef936b82f0ea706fa634bedeaa5043106
daniel <daalfonso@uniempresarial.edu.co>
2026-09-06 11:42:35 -0500

    correccion errores de drift
    se corre guio un error a la hora de drift ya que nunca se activaba la función

--- scripts/player/playerkart.gd
+++ scripts/player/playerkart.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 @export_group("Fisicas del kart")
-@export var max_speed: float = 20.0
+@export var max_speed: float = 50.0
 @export var acceleration: float = 15.0
 @export var friction: float = 8.0
 @export var steering_angle: float = 2.5
@@ -13,14 +13,16 @@ extends CharacterBody3D
 
 @export_group("Derrape")
 @export var jump_force: float = 8.0
-@export var drift_steering: float = 2.0
-@export var boost_impulse: float = 2000.0
+@export var drift_steering: float = 1.5
+@export var boost_impulse: float = 15.0
+@export var boost_duration: float = 1.0
 
 var current_speed: float = 0.0
 var current_steering: float = 0.0
 var is_drifting: bool = false
 var drift_dir: float = 0.0
 var drift_timer: float = 0.0
+var boost_timer: float = 0.0
 
 @onready var item_roulette = $ItemRoulette
 
@@ -104,21 +106,25 @@ func _physics_process(delta: float) -> void:
 	var turn_input :float = Input.get_action_strength("girar_izquierda")-Input.get_action_strength("girar_derecha")
 	var acceleration_input :float = Input.get_action_strength("acelerar")-Input.get_action_strength("frenar")
 	
-	if acceleration_input !=0:
-		current_speed = move_toward(current_speed, acceleration_input * max_speed, acceleration * delta)
+	if boost_timer > 0.0:
+		boost_timer -= delta
+		current_speed = max_speed + boost_impulse
 	else:
-		current_speed = move_toward(current_speed, 0.0, friction * delta)
+		if acceleration_input !=0:
+			current_speed = move_toward(current_speed, acceleration_input * max_speed, acceleration * delta)
+		else:
+			current_speed = move_toward(current_speed, 0.0, friction * delta)
 	
 	_handle_drift(turn_input,delta)
 	
 	current_steering = lerp(current_steering, turn_input, steering_smooth * delta)
 	
 	if abs(current_speed) > 0.1:
-		var speed_factor :float = clamp(abs(current_speed) / max_speed, 0.3, 1.0)
+		var speed_factor: float = clamp(abs(current_speed) / max_speed, 0.3, 1.0)
 		
 		if is_drifting:
-			var total_drift_turn: float = (drift_dir * drift_steering) + (current_steering * 0.5)
-			rotate_y(total_drift_turn * delta * sign(current_speed))
+			var drift_turn: float = (drift_dir * 0.8) + (turn_input * 0.4)
+			rotate_y(drift_turn * drift_steering * delta * sign(current_speed))
 		else:
 			rotate_y(current_steering * steering_speed * speed_factor * delta * sign(current_speed))
 	
@@ -141,21 +147,28 @@ func _physics_process(delta: float) -> void:
 		spring_arm.global_transform.basis = new_basis.orthonormalized()
 
 	
-func  _handle_drift(turn_input: float, delta: float):
-	if Input.is_action_just_pressed("derrapar") and is_on_floor() and abs(turn_input) > 0.2 and current_speed > 5.0:
+func _handle_drift(turn_input: float, delta: float):
+	if Input.is_action_just_pressed("derrapar") and is_on_floor():
 		is_drifting = true
-		drift_dir = sign(turn_input)
+		drift_dir = sign(turn_input) if turn_input != 0 else 1.0
 		drift_timer = 0.0
 		velocity.y = jump_force
+		print("¡Derrape Iniciado!")
 	
+
 	if is_drifting:
-		if Input.is_action_just_pressed("derrapar") and is_on_floor():
+		if Input.is_action_pressed("derrapar"):
 			drift_timer += delta
+			print("Tiempo derrapando: ", drift_timer)
 		else:
 			_end_drift()
+
 func _end_drift():
-	if drift_timer >= 1.0:
-		current_speed = min(current_speed + boost_impulse, max_speed + boost_impulse)
+	if drift_timer >= 0.4:
+		boost_timer = boost_duration
+		print("¡SE ACTIVÓ TURBO!")
+	else:
+		print("Derrape muy corto, no hubo turbo")
 	
 	is_drifting = false
 	drift_dir = 0.0
